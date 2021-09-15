df8332ba7960e12091a25306809effb41671eb00
jboskovic <jboskovic97@gmail.com>
2021-09-15 23:26:18 +0200

    Popravljeno iscrtavanje grafa, promenjene boje grana u zavisnoti da li se crta graf, spanning tree ili inverse, popravljen ispis tezine grafa

--- src/visualise.py
+++ src/visualise.py
@@ -5,13 +5,38 @@ import math
 from pathlib import Path
 from main import activate
 
-windowWidth = 750
+
+
+windowWidth = 800
 windowHeight = 400
 
 canvasWidth = 950
 canvasHeight = 800
 
+bgCanvasColor = "gray"
+bgTopLevelColor = "white"
+linesGraphColor = "black"
+linesTreeColor = "red"
+linesInvColor = "orange"
+nodeColor = "blue"
+textColor = "black"
+bgTextColor = "white"
+weigthTextColor = "black"
+nodeBorderColor = "black"
+lineActiveColor = "white"
+
+
 titleStyle = ("Times", 20, "bold")
+
+def onWeightClick(event, arg):
+    canvas = arg[0]
+    id = arg[1]
+    num = canvas.itemcget(id,'text')
+    num = int(num) +1
+    canvas.itemconfigure(id, text= num )
+
+
+
 
 #ne koristi se nigde, pravi string od instrukcija bloka koji je prosledjen
 def getInstructionsText(block):
@@ -28,12 +53,13 @@ def createNode(x, y, r, nodeText, canvasName):
     x1 = x + r
     y1 = y + r
     text = canvasName.create_text((x, y), text=nodeText)
-    canvasName.create_oval(x0, y0, x1, y1, fill='red')
+    canvasName.create_oval(x0, y0, x1, y1, fill=nodeColor, outline=nodeBorderColor, width=2)
     return canvasName.tag_raise(text)
 
 #MOZE SE UNAPREDITI, da se postavi da lepse crta tezine i grane
 #canvas crta granu od begining cvora do end cvora za weigth tezinom
-def createLink(begining, end, weigth, canvasName, putWeigths = True):
+def createLink(begining, end, weigth, canvasName, color, putWeigths = True):
+
     Ax,Ay = begining
     Bx, By = end
     d = math.sqrt((Bx-Ax)**2 + (By-Ay)**2)    
@@ -42,13 +68,30 @@ def createLink(begining, end, weigth, canvasName, putWeigths = True):
 
     newx2 = Ax + (d-20)/d*(Bx-Ax)
     newy2 = Ay + (d-20)/d*(By-Ay)
-   
-    if(putWeigths):
-        textX = Ax + 1/2*(Bx-Ax)
-        textY = Ay + 1/2*(By-Ay)
-        text = canvasName.create_text((textX, textY), text=str(weigth))
 
-    return canvasName.create_line(newx1, newy1, newx2, newy2, arrow=tk.LAST)
+
+    if(putWeigths):
+        # move text above line
+        # textX, TextY represent middle of a line
+        # computes line that is normal to our line then computes coordinates on that line
+        # 1 + k1*k2 = 0 lines are normal
+        textX = Ax + 1 / 2 * (Bx - Ax)
+        textY = Ay + 1 / 2 * (By - Ay)
+        if Ay == By:
+            textY = textY + 8
+        elif Ax == Bx:
+            textX = textX - 8
+        else:
+            k1 = (Ax-Bx)/(Ay-By)
+            k2 = -1/k1
+            n2 = textY - k2*textX
+            textY = textY - 5
+            textX = (textY-n2)/k2
+
+        weightObj = canvasName.create_text((textX, textY), text=str(weigth), fill=weigthTextColor)
+        canvasName.tag_bind(weightObj, '<Double-1>', lambda event, arg=[canvasName,weightObj]: onWeightClick(event,arg))
+
+    return canvasName.create_line(newx1, newy1, newx2, newy2, arrow=tk.LAST, fill=color, activefill=lineActiveColor)
 
 #MOZE SE UNAPREDITI, pozicije cvorova
 #crta cvorove i vraca njihove pozicije
@@ -62,30 +105,30 @@ def drawGraph(canvas, graph):
         size = 20
         if(k in [1,2]):
             currentX = canvasWidth/2
-            currentY =  heightFromTop * int(k)
+            currentY =  heightFromTop * (1.5*k-0.5)
         elif (k in [3,4]):
-            currentY = heightFromTop * 3 - (k-3)*20
-            currentX = float(canvasWidth)/7 * k
+            currentY = heightFromTop * 4 - (k-3)*18
+            currentX = float(canvasWidth)/7 * (7-k)
         elif(k in [5,6]):
-            currentY = heightFromTop * 4
+            currentY = heightFromTop * 5.5
             currentX = float(canvasWidth)/7 * (k-2)
         elif(k in [7,8]):
-            currentY = heightFromTop * 5
+            currentY = heightFromTop * 7
             currentX = float(canvasWidth)/7 * (k-4)
         elif(k in [9,10]):
-            currentY = heightFromTop * 6
+            currentY = heightFromTop * 9
             currentX = float(canvasWidth)/7 * (k-6)
         elif(k == 11):
-            currentY = heightFromTop * 7
+            currentY = heightFromTop * 10.5
             currentX = canvasWidth/2 
         elif(k in [12,13]):
-            currentY = heightFromTop * 8
+            currentY = heightFromTop * 11
             currentX = float(canvasWidth)/7 * (k-9)
         elif(k in [14,15]):
-            currentY = heightFromTop * 9
+            currentY = heightFromTop * 12.5
             currentX = float(canvasWidth)/7 * (k-11)
         elif(k in [16,17]):
-            currentY = heightFromTop * 10 + 20*(k-16)
+            currentY = heightFromTop * 12 + 20*(k-16)
             currentX = float(canvasWidth)/7 * (k-13)
         elif(k =='START'):
             currentX = 50
@@ -104,7 +147,8 @@ def drawGraph(canvas, graph):
     return positions
 
 #vraca listu veza(grana) izmedju cvorova
-def drawLinks(canvas, positions, graph, drawWeights=True):
+def drawLinks(canvas, positions, graph, color ,drawWeights=True):
+
     links = []
     for k,v in graph.items():
         try:
@@ -113,7 +157,7 @@ def drawLinks(canvas, positions, graph, drawWeights=True):
             for item in v:
                 #cvor u koji ulazi grana
                 endDimensions = positions[item[0]]
-                links.append(createLink(startDimesions, endDimensions,item[1], canvas, drawWeights))
+                links.append(createLink(startDimesions, endDimensions,item[1], canvas, color, drawWeights))
         except:
             print("Not found")
     return links
@@ -130,15 +174,20 @@ class mainWindow:
         #this list contains names of all test fies avalible
         #if you write new test file you need to include its name into this list
         #and then you will be able to choose that file to test
-        testFilesAvailable = {"block_test.py", "if_test.py", "test_input.py", "for_test.py"}
+        testFilesAvailable = ["block_test.py", "if_test.py", "test_input.py", "for_test.py", "write example"]
 
         #error labela (pritiskom na clear code pa na submit -> syntax error ...)
         self.errorLabel = tk.Label(root, fg='red', justify=tk.LEFT)
         codeTextWidth = 625
         i=1
         for fileName in testFilesAvailable:
-                btnFile = tk.Button(root, text = fileName, width=12,
+                if fileName == "write example":
+                    # write your own example
+                    btnFile = tk.Button(root, text=fileName, width=12, command=(lambda text=fileName: self.chooseFile("")))
+                else:
+                    btnFile = tk.Button(root, text = fileName, width=12,
                                  command = (lambda text=fileName: self.chooseFile(text)))
+
                 btnFile.place(x=20+codeTextWidth , rely =(10+60*i)/windowWidth)
                 i+=1
 
@@ -178,9 +227,13 @@ class mainWindow:
         #proporcija prozora za text
         codeText.place(height = 400-windowHeight/10, width = 600, relx=10/windowWidth, rely=10/windowHeight)
         #zakucan primer za submitovanje
-        test_file = open(Path(fileName), 'r')
-        #na kraju prozora za text upisuje se primer(inicijalno je kraj na pocetku)
-        codeText.insert(tk.END, test_file.read())
+        if fileName != "":
+            test_file = open(Path(fileName), 'r')
+            #na kraju prozora za text upisuje se primer(inicijalno je kraj na pocetku)
+            codeText.insert(tk.END, test_file.read())
+        else:
+            # if file isnt sent then you should write you own example
+            codeText.insert(tk.END, "")
         return codeText
 
     # menja opis i naslov grafa
@@ -191,8 +244,8 @@ class mainWindow:
 
     #definisanje opisa i naslova grafa
     def drawGraphTextAndTitle(self, text, title):
-        self.graphDescription =  tk.Text(self.graphWindow, width=32, wrap=tk.WORD)
-        self.graphTitle = tk.Label(self.graphWindow,font=titleStyle, text=title)
+        self.graphDescription = tk.Text(self.graphWindow, width=32, wrap=tk.WORD, bg=bgTextColor)
+        self.graphTitle = tk.Label(self.graphWindow,font=titleStyle, text=title, bg=bgTextColor)
         self.graphDescription.place(x=canvasWidth+10, y=150)
         self.graphTitle.place(x=canvasWidth+ 10, y = 50)
         self.graphDescription.insert(tk.END, text)
@@ -204,7 +257,7 @@ class mainWindow:
         #opis grafa i naslov
         self.drawGraphTextAndTitle(cfgDesc, "Graf kontrole toka")
         #crta grane cfg-a (false znaci da nece crtati tezine)
-        self.lines = drawLinks(self.canvas, self.positions, self.graph, False)
+        self.lines = drawLinks(self.canvas, self.positions, self.graph, linesGraphColor , False)
         #dugme za generisanje razapinjuceg stabla, pritiskom se poziva drawSpanningTree()
         self.nextBtn = tk.Button(self.graphWindow, text="Generiši razapinjujuće stablo", command=(lambda: self.drawSpanningTree()))
         self.nextBtn.place(x=canvasWidth+20, y=650)
@@ -217,7 +270,7 @@ class mainWindow:
         #brise sve grane
         deleteAllLinks(self.canvas, self.lines)
         #crta grane razapinjuceg grafa (false znaci da nece crtati tezine)
-        self.lines = drawLinks(self.canvas, self.positions, self.spanning_tree, False)
+        self.lines = drawLinks(self.canvas, self.positions, self.spanning_tree, linesTreeColor, False)
         #brise konfiguraciju za nextBtn i postavlja novu, pritiskom izvrsava self.drawInverseSpaningTreeNoWeights()
         self.nextBtn.place_forget()
         self.nextBtn = tk.Button(self.graphWindow, text="Generiši inverz stabla", command=(lambda: self.drawInverseSpaningTreeNoWeights()))
@@ -231,7 +284,7 @@ class mainWindow:
         #brise prethodno nacrtane grane
         deleteAllLinks(self.canvas, self.lines)
         #crta grane inverza bez tezina
-        self.lines = drawLinks(self.canvas, self.positions, self.inv_spanning_tree, False)
+        self.lines = drawLinks(self.canvas, self.positions, self.inv_spanning_tree, linesInvColor, False)
         #brise konfiguraciju za dugme i dodaje novu, pritiskom izvrsava metodu drawInverseSpaningTree()
         self.nextBtn.place_forget()
         self.nextBtn = tk.Button(self.graphWindow, text="Dodaj težine", command=(lambda: self.drawInverseSpaningTree()))
@@ -245,7 +298,7 @@ class mainWindow:
         #brise grane
         deleteAllLinks(self.canvas, self.lines)
         #crta grane inverznog grafa sa tezinama
-        self.lines = drawLinks(self.canvas, self.positions, self.inv_spanning_tree, True)
+        self.lines = drawLinks(self.canvas, self.positions, self.inv_spanning_tree, linesInvColor, True)
         # brise konfiguraciju za dugme i dodaje novu, pritiskom izvrsava metodu drawList(0) - dodaje tezine sa ostale grane(jednu po jednu)
         self.nextBtn.place_forget()
         self.nextBtn = tk.Button(self.graphWindow, text="Dodaj težine za ostale grane", command=(lambda: self.drawList(0)))
@@ -259,7 +312,7 @@ class mainWindow:
         if(i < numOfSteps):
 
             deleteAllLinks(self.canvas, self.lines)
-            self.lines = drawLinks(self.canvas, self.positions, self.list[i], True)
+            self.lines = drawLinks(self.canvas, self.positions, self.list[i], linesInvColor ,True)
             self.nextBtn.place_forget()
             self.nextBtn = tk.Button(self.graphWindow, text="Dodaj težinu za sledeću granu" , command=(lambda: self.drawList(i+1)))
             self.nextBtn.place(x=canvasWidth+20, y=650)
@@ -269,14 +322,13 @@ class mainWindow:
 
     #otvara prozor za vizuelizaciju algoritma
     def openNewWindow(self): 
-        newWindow = tk.Toplevel() 
-        newWindow.title("Vizuelizacija knutovog algoritma") 
-        newWindow.geometry("1280x700")
-        self.graphWindow = newWindow 
+        self.newWindow = tk.Toplevel(bg=bgTopLevelColor)
+        self.newWindow.title("Vizuelizacija knutovog algoritma")
+        self.newWindow.geometry("1280x700")
+        self.graphWindow = self.newWindow
 
         #postavlja odgovarajuce parametre za crtanje: dimenzije,boju,prozor za crtanje
-        self.canvas = tk.Canvas(self.graphWindow, bg="gray", height=canvasHeight, width=canvasWidth)
-
+        self.canvas = tk.Canvas(self.graphWindow, bg=bgCanvasColor, height=canvasHeight, width=canvasWidth)
         #prosledjuje graf za crtanje, vraca pozicije cvorova cfg-a
         self.positions = drawGraph(self.canvas, self.graph)
         
